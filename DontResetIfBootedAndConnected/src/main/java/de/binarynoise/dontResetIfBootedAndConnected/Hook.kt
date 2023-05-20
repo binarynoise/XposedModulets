16ed8a6c7397db3d64d0546dcd2808a3491c476a
binarynoise <binarynoise@mail.de>
2023-05-21 01:02:19 +0200

    create DontResetIfBootedAndConnected

---
+++ DontResetIfBootedAndConnected/src/main/java/de/binarynoise/dontResetIfBootedAndConnected/Hook.kt
@@ -0,0 +1,28 @@
+package de.binarynoise.dontResetIfBootedAndConnected
+
+import android.annotation.SuppressLint
+import android.util.Log
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+@SuppressLint("PrivateApi", "MissingPermission")
+class Hook : IXposedHookLoadPackage {
+    
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XposedHelpers.findAndHookMethod(
+            "com.android.server.StorageManagerService",
+            lpparam.classLoader,
+            "resetIfBootedAndConnected",
+            object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    param.result = Unit
+                    Log.i(
+                        "StorageManagerService", "If I wasn't told not to, I would've resetIfBootedAndConnected now."
+                    )
+                }
+            },
+        )
+    }
+}
