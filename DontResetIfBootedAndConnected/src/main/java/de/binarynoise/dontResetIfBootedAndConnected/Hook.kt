bfc6e8ff8cd21e24c9555b2b0512f485a8d3f1fd
binarynoise <binarynoise@mail.de>
2023-06-17 17:28:04 +0200

    catch ClassNotFoundError and NoSuchMethodError

--- DontResetIfBootedAndConnected/src/main/java/de/binarynoise/dontResetIfBootedAndConnected/Hook.kt
+++ DontResetIfBootedAndConnected/src/main/java/de/binarynoise/dontResetIfBootedAndConnected/Hook.kt
@@ -5,24 +5,27 @@ import android.util.Log
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedHelpers.ClassNotFoundError
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 @SuppressLint("PrivateApi", "MissingPermission")
 class Hook : IXposedHookLoadPackage {
     
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookMethod(
-            "com.android.server.StorageManagerService",
-            lpparam.classLoader,
-            "resetIfBootedAndConnected",
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    param.result = Unit
-                    Log.i(
-                        "StorageManagerService", "If I wasn't told not to, I would've resetIfBootedAndConnected now."
-                    )
-                }
-            },
-        )
+        try {
+            XposedHelpers.findAndHookMethod(
+                "com.android.server.StorageManagerService",
+                lpparam.classLoader,
+                "resetIfBootedAndConnected",
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        param.result = Unit
+                        Log.i("StorageManagerService", "If I wasn't told not to, I would've resetIfBootedAndConnected now.")
+                    }
+                },
+            )
+        } catch (_: ClassNotFoundError) {
+        } catch (_: NoSuchMethodError) {
+        }
     }
 }
