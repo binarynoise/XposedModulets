f658844f75193b0dd6ea473eeee23a41aaf8fe7a
binarynoise <binarynoise@mail.de>
2023-10-02 22:22:11 +0200

    dependency update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(kotlin("bom"))
-    implementation("com.android.tools.build:gradle:8.1.1")
+    implementation("com.android.tools.build:gradle:8.1.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     implementation("org.kohsuke:github-api:1.316")
 }
