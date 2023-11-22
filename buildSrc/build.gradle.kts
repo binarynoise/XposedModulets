ae8301e59b7410a78937df7b02fa5ce228f86280
binarynoise <binarynoise@mail.de>
2023-11-22 22:29:57 +0100

    update gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(kotlin("bom"))
-    implementation("com.android.tools.build:gradle:8.1.3")
+    implementation("com.android.tools.build:gradle:8.1.4")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
 //    implementation("org.jetbrains.kotlin:kotlin-serialization")
     implementation("org.kohsuke:github-api:1.316")
--- buildSrc/src/main/kotlin/common.gradle.kts
+++ buildSrc/src/main/kotlin/common.gradle.kts
@@ -201,7 +201,7 @@ if (isAndroid || isAndroidLib) {
 
 if (isKotlinLib || isKotlinAndroid) {
     dependencies {
-        add("implementation", "org.jetbrains:annotations:24.0.1")
+        add("implementation", "org.jetbrains:annotations:24.1.0")
     }
 }
 
