e221e4b472e958c37aa07311035fe2cfbe3f430e
binarynoise <binarynoise@mail.de>
2025-04-02 22:59:54 +0200

    fix Bluetooth device sorting for large lists of Bluetooth devices
    not doing IPC is remarkably quicker than doing it

--- BetterBluetoothDeviceSort/src/main/java/de/binarynoise/betterBluetoothDeviceSort/Hook.kt
+++ BetterBluetoothDeviceSort/src/main/java/de/binarynoise/betterBluetoothDeviceSort/Hook.kt
@@ -1,7 +1,8 @@
 package de.binarynoise.betterBluetoothDeviceSort
 
-import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import de.binarynoise.logger.Logger.log
+import de.binarynoise.reflection.cast
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -10,13 +11,36 @@ import de.robv.android.xposed.XC_MethodHook as MethodHook
 class Hook : IXposedHookLoadPackage {
     
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookMethod(BluetoothAdapter::class.java, "getMostRecentlyConnectedDevices", object : MethodHook() {
-            override fun afterHookedMethod(param: MethodHookParam) {
-                with(param) {
-                    @Suppress("UNCHECKED_CAST") val unsorted = result as MutableList<BluetoothDevice>
-                    unsorted.sortBy { a -> a.alias ?: a.name }
+        
+        XposedHelpers.findAndHookMethod(
+            "com.android.bluetooth.btservice.storage.DatabaseManager", lpparam.classLoader, "getMostRecentlyConnectedDevices",
+            object : MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    log("called getMostRecentlyConnectedDevices")
+                    with(param) {
+                        if (throwable != null || result == null) return
+                        @Suppress("UNCHECKED_CAST") val unsorted = result as MutableList<BluetoothDevice>
+                        log("${unsorted.size} devices")
+                        
+                        val mAdapterService = XposedHelpers.getObjectField(param.thisObject, "mAdapterService")
+                        val mRemoteDevices = XposedHelpers.getObjectField(mAdapterService, "mRemoteDevices")
+                        val map = unsorted.associateWith { XposedHelpers.callMethod(mRemoteDevices, "getDeviceProperties", it) }
+                        
+                        val mMetadataCache = XposedHelpers.getObjectField(param.thisObject, "mMetadataCache").cast<Map<String, *>>()
+                        val (bonded, others) = unsorted.partition { mMetadataCache.containsKey(it.address) }
+                        log("${bonded.size} bonded, ${others.size} others")
+                        
+                        val comparator = Comparator.comparing<BluetoothDevice, String>({
+                            val properties = map[it] ?: return@comparing null
+                            (XposedHelpers.getObjectField(properties, "mAlias") ?: XposedHelpers.getObjectField(properties, "mName")).cast<String?>()
+                        }, Comparator.nullsLast(Comparator.naturalOrder<String>()))
+                        
+                        result = bonded.toSortedSet(comparator).toMutableList() + others
+                    }
                 }
-            }
-        })
+            },
+        )
+        
+        log("Hooked getMostRecentlyConnectedDevices")
     }
 }
