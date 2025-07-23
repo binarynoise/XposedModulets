c2273b88769d72d99056ab324350ef318816afc1
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2025-07-23 23:07:13 +0200

    make AlwaysAllowChargingSounds respect the user settings

--- AlwaysAllowChargingSounds/src/main/java/de/binarynoise/AlwaysAllowChargingSounds/Hook.kt
+++ AlwaysAllowChargingSounds/src/main/java/de/binarynoise/AlwaysAllowChargingSounds/Hook.kt
@@ -1,15 +1,24 @@
 package de.binarynoise.AlwaysAllowChargingSounds
 
+import android.content.Context
+import android.provider.Settings
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class Hook : IXposedHookLoadPackage {
-
+    
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         val NotifierClass = XposedHelpers.findClass("com.android.server.power.Notifier", lpparam.classLoader)
-        XposedHelpers.findAndHookMethod(NotifierClass, "isChargingFeedbackEnabled", Int::class.java, XC_MethodReplacement.returnConstant(true))
+        XposedHelpers.findAndHookMethod(NotifierClass, "isChargingFeedbackEnabled", Int::class.java, object : XC_MethodReplacement() {
+            override fun replaceHookedMethod(param: MethodHookParam): Boolean {
+                val userId = param.args[0] as Int
+                val context = XposedHelpers.getObjectField(param.thisObject, "mContext") as Context
+                return XposedHelpers.callStaticMethod(
+                    Settings.Secure::class.java, "getIntForUser", context.contentResolver, "charging_sounds_enabled", 1, userId
+                ) != 0
+            }
+        })
     }
-
 }
