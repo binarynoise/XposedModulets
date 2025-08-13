cd5a8c5c273afa4cdb6352a567a07a905fd8e858
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2025-08-13 10:59:26 +0200

    Implement SensorMod

---
+++ SensorMod/src/main/java/com/programminghoch10/SensorMod/SettingsActivity.java
@@ -0,0 +1,60 @@
+package com.programminghoch10.SensorMod;
+
+import static com.programminghoch10.SensorMod.Common.DEFAULT_SENSOR_STATE;
+import static com.programminghoch10.SensorMod.Common.getKey;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+import java.util.List;
+
+public class SettingsActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(
+                    getSupportFragmentManager().getBackStackEntryCount() > 0
+            );
+        }
+    }
+    
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @SuppressLint("WorldReadableFiles")
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            PreferenceManager preferenceManager = getPreferenceManager();
+            preferenceManager.setSharedPreferencesName("sensors");
+            preferenceManager.setSharedPreferencesMode(MODE_WORLD_READABLE);
+            PreferenceScreen preferenceScreen = getPreferenceScreen();
+            SensorManager sensorManager = (SensorManager) requireContext().getSystemService(SENSOR_SERVICE);
+            List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+            for (Sensor sensor : sensors) {
+                SwitchPreference preference = new SwitchPreference(requireContext());
+                preference.setDefaultValue(DEFAULT_SENSOR_STATE);
+                preference.setKey(getKey(sensor));
+                preference.setTitle(sensor.getName());
+                preference.setSummary(getKey(sensor) + " - " + sensor);
+                preferenceScreen.addPreference(preference);
+            }
+        }
+    }
+}
