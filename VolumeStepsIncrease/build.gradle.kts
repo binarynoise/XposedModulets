b21e660eb1230e0f0a80800498876fb1ed1a56f6
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2025-08-16 13:49:47 +0200

    Implement VolumeStepsIncrease

---
+++ VolumeStepsIncrease/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.buildlogic.android.application)
+    alias(libs.plugins.buildlogic.kotlin.android)
+}
+
+android {
+    namespace = "com.programminghoch10.VolumeStepsIncrease"
+    
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 35
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.preference)
+}
