db239284743d76b16f18f4db70f119b0eb670afd
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2025-08-27 00:22:17 +0200

    Add DisableScreenshotSounds

--- DisableSounds/build.gradle.kts
+++ DisableSounds/build.gradle.kts
@@ -9,5 +9,15 @@ android {
     defaultConfig {
         minSdk = 17
         targetSdk = 36
+        buildConfigField("String", "SHARED_PREFERENCES_NAME", "\"disable_sounds\"")
     }
+}
+
+dependencies {
+    // fragment-ktx is included as transitive dependency through preference-ktx
+    // the transitive dependency is a lower version though, which allows minSdk 17,
+    // while explicit mention with the latest version forced minSdk 21
+    //implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.preference.ktx)
+    implementation(libs.kotlinx.coroutines.guava)
 }
