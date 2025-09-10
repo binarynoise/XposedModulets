27505fb0628052e05dd06ab959748a14667a1b0c
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2025-09-10 15:19:55 +0200

    add GalaxyWearable

---
+++ GalaxyWearable/src/main/java/com/programminghoch10/GalaxyWearable/Hook.java
@@ -0,0 +1,14 @@
+package com.programminghoch10.GalaxyWearable;
+
+import android.os.Build;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Hook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "");
+    }
+}
