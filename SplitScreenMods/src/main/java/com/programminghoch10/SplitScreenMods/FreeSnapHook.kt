213040d7b36b81ed5c954aab212a8e4675548bd2
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2025-11-12 15:14:06 +0100

    implement freesnap splitscreen mod

---
+++ SplitScreenMods/src/main/java/com/programminghoch10/SplitScreenMods/FreeSnapHook.kt
@@ -0,0 +1,37 @@
+package com.programminghoch10.SplitScreenMods
+
+import android.os.Build
+import com.programminghoch10.SplitScreenMods.BuildConfig.SHARED_PREFERENCES_NAME
+import com.programminghoch10.SplitScreenMods.FreeSnapHookConfig.enabled
+import de.binarynoise.logger.Logger.log
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+object FreeSnapHookConfig {
+    @JvmField
+    val enabled = Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+}
+
+class FreeSnapHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName != "com.android.systemui") return
+        if (!enabled) return
+        log("handleLoadPackage(${lpparam.packageName} in process ${lpparam.processName})")
+        val preferences = XSharedPreferences(BuildConfig.APPLICATION_ID, SHARED_PREFERENCES_NAME)
+        val enabled = preferences.getBoolean("FreeSnap", false)
+        if (!enabled) return
+        
+        val DividerSnapAlgorithmClass =
+            XposedHelpers.findClass("com.android.wm.shell.common.split.DividerSnapAlgorithm", lpparam.classLoader)
+        XposedBridge.hookAllConstructors(DividerSnapAlgorithmClass, object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                XposedHelpers.setBooleanField(param.thisObject, "mFreeSnapMode", true)
+                log("${DividerSnapAlgorithmClass.simpleName} mFreeSnapMode constant changed to true")
+            }
+        })
+    }
+}
