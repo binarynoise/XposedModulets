b032d49363223e16024bb0d6da6c76f8ae97c9bf
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-01-13 09:53:09 +0100

    GalaxyWearable: add disable forced update check

--- GalaxyWearable/src/main/java/com/programminghoch10/GalaxyWearable/DisableManufacturerCheckHook.java
+++ GalaxyWearable/src/main/java/com/programminghoch10/GalaxyWearable/DisableManufacturerCheckHook.java
@@ -6,7 +6,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class Hook implements IXposedHookLoadPackage {
+public class DisableManufacturerCheckHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
         XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "");
