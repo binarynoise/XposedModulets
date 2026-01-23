bff006a1e6a32efc9bb5119f3f51a4fcd481d4d7
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2026-01-23 11:02:25 +0100

    fix AntiWakeLock PowerManager Hook targeting the wrong class

--- AntiWakeLock/src/main/java/com/programminghoch10/AntiWakeLock/PowerManagerHook.kt
+++ AntiWakeLock/src/main/java/com/programminghoch10/AntiWakeLock/PowerManagerHook.kt
@@ -12,23 +12,23 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class PowerManagerHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedHelpers.findAndHookMethod(PowerManager::class.java, "acquire", DO_NOTHING)
-        XposedHelpers.findAndHookMethod(PowerManager::class.java, "acquire", Int::class.java, DO_NOTHING)
+        XposedHelpers.findAndHookMethod(PowerManager.WakeLock::class.java, "acquire", DO_NOTHING)
+        XposedHelpers.findAndHookMethod(PowerManager.WakeLock::class.java, "acquire", Int::class.java, DO_NOTHING)
         
         // optional hooks for completeness
-        XposedHelpers.findAndHookMethod(PowerManager::class.java, "isHeld", returnConstant(false))
-        XposedHelpers.findAndHookMethod(PowerManager::class.java, "release", DO_NOTHING)
-        XposedHelpers.findAndHookMethod(PowerManager::class.java, "release", Int::class.java, DO_NOTHING)
-        XposedHelpers.findAndHookMethod(PowerManager::class.java, "setReferenceCounted", Boolean::class.java, DO_NOTHING)
+        XposedHelpers.findAndHookMethod(PowerManager.WakeLock::class.java, "isHeld", returnConstant(false))
+        XposedHelpers.findAndHookMethod(PowerManager.WakeLock::class.java, "release", DO_NOTHING)
+        XposedHelpers.findAndHookMethod(PowerManager.WakeLock::class.java, "release", Int::class.java, DO_NOTHING)
+        XposedHelpers.findAndHookMethod(PowerManager.WakeLock::class.java, "setReferenceCounted", Boolean::class.java, DO_NOTHING)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) XposedHelpers.findAndHookMethod(
-            PowerManager::class.java,
+            PowerManager.WakeLock::class.java,
             "setStateListener",
             Executor::class.java,
             PowerManager.WakeLockStateListener::class.java,
             DO_NOTHING,
         )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) XposedHelpers.findAndHookMethod(
-            PowerManager::class.java,
+            PowerManager.WakeLock::class.java,
             "setWorkSource",
             WorkSource::class.java,
             DO_NOTHING,
